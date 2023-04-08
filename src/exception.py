8e1bdcf8a538b3f2c2e25f3e2649ad878df3b24c
blaze123441 <RAHUL23082001JHA@gmail.com>
2023-04-08 13:20:43 +0530

    python fix

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+import logger
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
     file_name =exc_tb.tb_frame.f_code.co_filename
@@ -13,13 +14,4 @@ class CustomException(Exception):
         self.error_message =  error_message_detail(error_message,error_detail=error_detail)
     def __str__(self) -> str:
         return self.error_message    
-
-if __name__ == "__main__":
-
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Logging has started")
-        raise  CustomException
-    finally:
-        logging.info(CustomException(e,sys))    
+    
--- src/logger.py
+++ src/logger.py
@@ -4,13 +4,13 @@ from datetime import datetime
 
 LOG_FILE = f'{datetime.now().strftime("%m_%d_%y_%H_%M_%S")}.log'
 logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.markdirs(logs_path,exist_ok = True)
+os.makedirs(logs_path,exist_ok = True)
 
 LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename= LOG_FILE_PATH,
     force="[ %(asctime)s] %(linemo)d %(name)s - %(levelname)s - %(message)s",
-    level= logging.INFO,
+    level= logging.INFO
 )
 
