0480d54f67ca7505082d752dec47393488228817
blaze123441 <RAHUL23082001JHA@gmail.com>
2023-06-28 18:20:40 +0530

    Fixed bugs

--- src/exception.py
+++ src/exception.py
@@ -16,12 +16,4 @@ class CustomException(Exception):
     def __str__(self) -> str:
         return self.error_message    
 
-if __name__ == "__main__":
-
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Logging has started")
-        raise  CustomException
-    finally:
-        logging.info(CustomException(e,sys))    
+          
