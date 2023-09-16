85cbe69a86a9d509a95fc8e422bb3f42c828fc9a
rahul-jha-2001 <RAHUL23082001JHA@gmail.com>
2023-09-16 20:38:38 +0530

    gradio done

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from src.Pipeline.predict_pipe import  Predict_Pipeline
+import gradio as gr
+from src.exception import CustomException
 
-if __name__ == "__main__":
-    obj = Predict_Pipeline()
-    print(obj.predict(gender = "female",
-        race_ethnicity="group C",
-        parental_level_of_education="bachelor's degree",
-        lunch= "free/reduced",
-        test_preparation_course="none",
-        reading_score= 75,
-        writing_score= 60))
+obj = Predict_Pipeline()
+
+demo = gr.Interface(obj.predict,
+    inputs= [
+        gr.Radio(["male","female"],label="Gender"),
+        gr.Dropdown(['group B' ,'group C' ,'group A' ,'group D' ,'group E'],label="Race"),
+        gr.Dropdown(["bachelor's degree" ,'some college' ,"master's degree", "associate's degree",
+ 'high school' ,'some high school'],label="Parent Education"),
+        gr.Radio(['standard', 'free/reduced'],label="Lunch"),
+        gr.Radio(['none', 'completed'],label="Coaching"),
+        gr.Slider(0,100,label="writing Score"),
+        gr.Slider(0,100,label="reading score")],
+        outputs = "number"
+            )   
+
+
+
+if __name__ == "__main__":   
+    demo.launch()
